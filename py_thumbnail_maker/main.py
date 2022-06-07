d7038db177a2549b16950b8c43f01920ffa79ecd
renmurakami <autoeyy@gmail.com>
2022-06-07 16:59:12 +0900

    code clean

--- py_thumbnail_maker/main.py
+++ py_thumbnail_maker/main.py
@@ -2,21 +2,8 @@ import os
 from img_picker import img_picker
 from py_thumbnail_maker.path_isExists import handleExists
 from resize import resize_img
-import asyncio
-
-
-print("hello dev")
-
-baseImgPath = "src/imgs"
-imgName = "E7AB8BE88AB1E99FBF"
-imgEx = "jpg"
-imgPath = f"{baseImgPath}/{imgName}.{imgEx}"
-
-distPath = "dist"
 
 img_list = img_picker()
-
-print(img_list)
 
 def run():
     for list in img_list:
