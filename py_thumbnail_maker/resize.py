1799a2002d4d4af13254a4c5aed6cab384d80ddb
renmurakami <autoeyy@gmail.com>
2022-06-07 23:21:13 +0900

    typo fix

--- py_thumbnail_maker/resize.py
+++ py_thumbnail_maker/resize.py
@@ -15,11 +15,13 @@ def resize_img(img_path: str, img_name: str, w: int, dist_path: str, resize_qual
         img = Image.open(img_path).convert("RGB")
     elif img_ex == "svg":
         img = Image.open(f"src/imgs{dist_path}/{img_name}.png",).convert("RGB")
+    elif img_ex == "psd":
+        img = Image.open(img_path, formats="psd")
     else:
         img = Image.open(img_path)
     img_w = img.width
     img_h = img.height
-    resize_raito = w / img_w
-    resize_size = (w, round(img_h * resize_raito))
+    resize_ratio = w / img_w
+    resize_size = (w, round(img_h * resize_ratio))
     resized_img = img.resize(resize_size)
     resized_img.save(f"{base_dist}{dist_path}/{img_name}.jpg", quantiles=resize_quality)
