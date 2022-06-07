d0ef142e6bdc9bd0f75cf423ff7dbb2dd8a4504a
renmurakami <autoeyy@gmail.com>
2022-06-07 18:50:54 +0900

    fix code

--- py_thumbnail_maker/path_to_img_info.py
+++ py_thumbnail_maker/path_to_img_info.py
@@ -3,7 +3,8 @@ def path_to_img_info(img_paths):
     for img_path in img_paths:
         base_split_img_path = img_path.split("/")
         base_len = len(base_split_img_path)
-        img_name = base_split_img_path[base_len - 1]
+        img_name_ex = base_split_img_path[base_len - 1]
+        img_name = img_name_ex.split(".")[0]
         remake_base_path_remove_src = img_path.split("src/imgs")[1]
         remake_base_path = remake_base_path_remove_src.split(".")[0]
         format_path_split = remake_base_path.split("/")
